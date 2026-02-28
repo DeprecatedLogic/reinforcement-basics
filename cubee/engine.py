16a276974d3a42f1e345ad8201b557162ff69e02
ItzKarizma <emertinaj2002@hotmail.com>
2026-02-28 03:39:15 +0100

    refactor(core): restructure architecture, extract enums, centralize movement logic and fix related wiring issues

--- cubee/engine.py
+++ cubee/engine.py
@@ -1,4 +1,7 @@
+from player import Player
 from model import GameModel
+from actions import Action
+from cell import Cell
 
 class GameEngine:
     """
@@ -11,21 +14,64 @@ class GameEngine:
     def __init__(self, model: GameModel):
         self.model = model
 
-    def make_move(self):
+    def is_valid_action(self, action: Action):
+        """
+        is_valid_action description
+        """
+        board = self.model.board
+
+        # Calculate the delta between player's position and action taken
+        delta_row, delta_column = action.delta
+        player_row = self.model.current_player.row + delta_row
+        player_column = self.model.current_player.column + delta_column
+
+        # Check if we're moving out of bounds or onto opponent's territory
+        if not board.is_within_bounds(player_row, player_column):
+            print(f"[GameEngine.is_valid_action] Action '{action.name}' is out of bounds")
+            return False
+        if board.grid[player_row][player_column] == self.model.get_opponent().cell:
+            print(f"[GameEngine.is_valid_action] Action '{action.name}' is invalid, cannot move onto opponent's territory")
+            return False
         
-        actions_available = []
+        return True
+        
+    def make_move(self):
+        """
+        make_move description
+        """
+
+        # Filter only valid actions
+        actions_available = [action for action in list(Action) if self.is_valid_action(action)]
         
         # player plays then we switch
-        action = self.model.current_player.play(actions_available)
+        action_taken = self.model.current_player.play(actions_available)
+
         self.switch_player()
 
-    def check_enclosure(self):
-        """ BFS implementation """
-        pass
-    
     def switch_player(self) -> None:
         """Get the next player."""
-        if self.model.current_player == self.players[0]:
-            self.model.current_player = self.players[1]
+        players = self.model.players
+        current_player = self.model.current_player
+        if current_player == players[0]:
+            current_player = players[1]
         else:
-            self.model.current_player = self.players[0]
+            current_player = players[0]
+
+    def get_winner(self) -> Player | None:
+        """ Return the winning player if the game is over, otherwise None. """
+        if not self.model.is_game_over():
+            return None
+        
+        player1, player2 = self.model.players
+        cells_counter = self.model.board.count_cells()
+        return player1 if cells_counter[Cell.P1] > cells_counter[Cell.P2] else player2
+
+    def get_loser(self) -> Player | None:
+        """ Return the losing player if the game is over, otherwise None. """
+        if not self.model.is_game_over():
+            return None
+        
+        player1, player2 = self.model.players
+        cells_counter = self.model.board.count_cells()
+        return player1 if cells_counter[Cell.P1] < cells_counter[Cell.P2] else player2
+    
--- cubee/player.py
+++ cubee/player.py
@@ -1,3 +1,5 @@
+from model import Cell
+from engine import Action
 from random import choice
 
 class Player:
@@ -6,66 +8,78 @@ class Player:
     Stores identity and statistics (wins/losses) and defines the interface for choosing an action.
     """
 
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         """
         Initialize a new player.
 
         Args:
             name: The player's display name
+            position: The player's position, row & column.
+
+        Note:
+            The parameter `position` should contain the row and column, not x and y.
         """
         self.name = name
         self.nb_wins = 0
         self.nb_losses = 0
-        pass
+        self.row = 0
+        self.column = 0
+        self.cell: Cell | None = None
 
-    @staticmethod
-    def play(actions_available: list):
+    def play(actions: list[Action]) -> Action:
         """
         Play description.
         
         Args:
-            actions_available: The actions a player can take in the current situation.
+            actions: The actions a player can take in the current situation.
 
         Returns:
             The action taken by the player.
         """
-        return choice(actions_available)
+        return choice(actions)
+
+    def reset_stats(self) -> None:
+        """ reset_stats description """
+        self.nb_wins = 0
+        self.nb_losses = 0
     
     @property
     def nb_games(self) -> int:
-        """Return the total number of games played by the player."""
+        """ Return the total number of games played by the player. """
         return self.nb_wins + self.nb_losses
     
 class Human(Player):
     """
-    Player controlled by user input (CLIR or GUI).
+    Player controlled by user input (CLI or GUI).
     Overrides the play method to request and validate user input.
     """
     
     def __init__(self, name):
         super().__init__(name)
 
-    def play(self, actions_available: list) -> int:
+    def play(self, actions: list[Action]) -> Action:
         """
-        Prompt the human player for the number of matches to remove.
+        Prompt the human player for the action to take.
 
         Note:
             Only called if the game is being played in CLI mode.
 
         Args:
-            actions_available: The actions a player can take in the current situation.
+            actions: The actions a player can take in the current situation.
             
         Returns:
-            Valid number of matches to .
+            The action the player took.
         """
         while True:
-            action_taken = input(
-                f"Enter the desired action (available: {str(actions_available).strip('[]')}): "
-            )
-            if action_taken in actions_available:
+            try:
+                action_taken = Action(input(
+                    f"Enter the desired action (available: {','.join([action.name for action in actions])}): "
+                ).upper().strip())
                 return action_taken
-            
-            print("Invalid Entry!")
+            except KeyError:
+                print(f"[Player.play] Invalid input, try again")
+            except Exception as e:
+                print(f"[Player.play] An uncaught error occured: {e}")
 
 class AI(Player):
     """
