bb8c9878cde253c187ba82c9802b09643bebd934
ItzKarizma <emertinaj2002@hotmail.com>
2026-03-15 20:55:11 +0100

    feat(model,player): implement model and player logic with fixes and improvements

--- cubee/player.py
+++ cubee/player.py
@@ -1,5 +1,6 @@
-from cubee.cell import Cell
+from cubee.cells import Cell
 from cubee.actions import Action
+from cubee.colors import Color
 from random import choice
 
 class Player:
@@ -7,19 +8,23 @@ class Player:
     Represents a generic player of the game.
     Stores identity and statistics (wins/losses) and defines the interface for choosing an action.
     """
+    player_names_used = []
 
-    def __init__(self, name) -> None:
+    def __init__(self, name: str, color: Color):
         """
         Initialize a new player.
 
         Args:
-            name: The player's display name
-            position: The player's position, row & column.
+            name (str): The player's display name
+            color (str): The player's color
 
         Note:
             The parameter `position` should contain the row and column, not x and y.
         """
+        if name in Player.player_names_used:
+            raise Exception(f"The name '{name}' is already in use, please enter another name.")
         self.name = name
+        self.color = color
         self.nb_wins = 0
         self.nb_losses = 0
         self.row = 0
@@ -44,9 +49,71 @@ class Player:
         self.nb_losses = 0
     
     @property
-    def nb_games(self) -> int:
-        """ Return the total number of games played by the player. """
+    def total_games(self) -> int:
+        """Return the total number of games played by the player.
+
+        Returns:
+            _type_: _description_
+        """
         return self.nb_wins + self.nb_losses
+    
+    @property
+    def color(self) -> None:
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self._color
+
+    @color.setter
+    def color(self, value: Color) -> None:
+        """_summary_
+
+        Args:
+            value (Color): _description_
+
+        Raises:
+            Exception: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if not isinstance(value, Color):
+            raise TypeError("Cannot set player's color to a value of type other than Color")
+
+        if value == Color.EMPTY:
+            raise ValueError("Cannot set player's color to the same color as the empty cells")
+
+        self._color = value
+
+    @property
+    def position(self) -> tuple[int, int]:
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.row, self.column
+
+    @position.setter
+    def position(self, value: tuple[int, int]) -> None:
+        """_summary_
+
+        Args:
+            value (tuple[int, int]): _description_
+        """
+        if not (
+            isinstance(value, tuple) and len(value) == 2 and
+            all(isinstance(element, int) for element in value)
+        ):
+            raise ValueError("What the F are you doing ? Take a break, drink a coffee, you need it.")
+        
+        self.row, self.column = value
+
+    def __str__(self) -> str:
+        """Return a string representation of self."""
+        return f"{self.name}({self.cell.name})"
     
 class Human(Player):
     """
@@ -54,8 +121,8 @@ class Human(Player):
     Overrides the play method to request and validate user input.
     """
     
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name: str, color: Color):
+        super().__init__(name, color)
 
     def play(self, actions: list[Action]) -> Action:
         """
@@ -65,10 +132,10 @@ class Human(Player):
             Only called if the game is being played in CLI mode.
 
         Args:
-            actions: The actions a player can take in the current situation.
+            actions (list[Action]): The actions a player can take in the current situation.
             
         Returns:
-            The action the player took.
+            Action: The action the player took.
         """
         while True:
             try:
@@ -86,5 +153,5 @@ class AI(Player):
     Automated Player.
     """
 
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name: str, color: Color):
+        super().__init__(name, color)
