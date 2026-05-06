21f4863dec9bf31c77cbe18eb14022e438cef565
ItzKarizma <emertinaj2002@hotmail.com>
2026-05-06 13:22:41 +0200

    chore: remove unnecessary files

--- pixel_kart/temp/const.py
+++
@@ -1,8 +0,0 @@
-### PixelKart
-
-PIXEL_TYPES = {
-        "ROAD":{"color":"grey", "letter":"R"},
-        "GRASS":{"color":"green", "letter":"G"},
-        "WALL":{"color":"black", "letter":"W"},
-        "FINISH":{"color":"yellow", "letter":"F"}
-    }
